c48cfb5e68419cb914ea002e7c28b4d7f44a69bc
Hossein_shirinegad <hosseinshirinegad.66@gmail.com>
2025-03-27 16:37:05 +0330

    created tasks and users table with seeder and factories

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(5)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Hossein',
+        //     'email' => 'hossein@gmail.com.com',
+        //     'password' => bcrypt('123456789'),
+        // ]);
+    }
+}
